d587850ba0ea9bcbefecd0f27f8fbbf91d56e7cc
ds20220914 <davidkuul2002@gmail.com>
2024-08-28 22:18:40 +0300

    tehtävä 1.9

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -41,6 +41,24 @@ const App = () => {
 	const [neutral, setNeutral] = useState(0)
 	const [bad, setBad] = useState(0)
 
+	if (good + neutral + bad === 0) {
+		return (
+			<div>
+				<h2>give feedback</h2>
+				<Button handleClick={() => setGood(good + 1)} text="good">
+					good
+				</Button>
+				<Button handleClick={() => setNeutral(neutral + 1)} text="neutral">
+					neutral
+				</Button>
+				<Button handleClick={() => setBad(bad + 1)} text="bad">
+					bad
+				</Button>
+				<h2>statistics</h2>
+				No statistics
+			</div>
+		)
+	}
 	return (
 		<div>
 			<h2>give feedback</h2>
