28a6c473dac544b615ac568c241b07b89ce82d3b
ds20220914 <davidkuul2002@gmail.com>
2024-09-12 00:32:48 +0300

    t2.5

--- osa2/sovellus/src/components/Course.jsx
+++ osa2/sovellus/src/components/Course.jsx
@@ -1,6 +1,37 @@
-import Header from "./Header"
-import Content from "./Content"
-import Total from "./Total"
+const Content = ({ parts }) => {
+	return (
+		<div>
+			{parts.map((part) => (
+				<Part key={part.id} osat={part} />
+			))}
+		</div>
+	)
+}
+
+const Header = (props) => {
+	return <h2>{props.course}</h2>
+}
+
+const Part = (props) => {
+	console.log(props.osat)
+	return (
+		<p>
+			{props.osat.name} {props.osat.exercises}
+		</p>
+	)
+}
+
+const Total = (props) => {
+	const totalExercises = props.total
+		.map((part) => part.exercises)
+		.reduce((sum, exercises) => sum + exercises, 0)
+
+	return (
+		<b>
+			<p>Number of exercises {totalExercises}</p>
+		</b>
+	)
+}
 
 const Course = ({ course }) => {
 	return (
