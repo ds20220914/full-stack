1426b59d4ac42030ee7cacdab13e9e8097c764d9
ds20220914 <davidkuul2002@gmail.com>
2024-09-21 23:36:33 +0300

    t3.2

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -32,6 +32,29 @@ app.get("/api/persons", (request, response) => {
 	response.json(notes)
 })
 
+app.get("/info", (request, response) => {
+	const length = notes.length
+	const now = new Date()
+
+	const formattedDate = now.toLocaleString("en-US", {
+		weekday: "short", // e.g., "Sat"
+		year: "numeric", // e.g., "2024"
+		month: "short", // e.g., "Jan"
+		day: "numeric", // e.g., "13"
+		hour: "numeric", // e.g., "9"
+		minute: "numeric", // e.g., "30"
+		second: "numeric", // e.g., "25"
+		timeZoneName: "long", // e.g., "Eastern European Standard Time"
+		timeZone: "Europe/Helsinki", // Set the Eastern European timezone
+	})
+	const responseText = `
+    <p>Phonebook has info for ${length} people</p>
+    <p>${formattedDate}</p>
+  `
+
+	response.send(responseText)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
